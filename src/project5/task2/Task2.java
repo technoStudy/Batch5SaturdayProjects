70cbd0df41a5e899982c74cb9d99bc7fb3c7cc21
techno.study <apps@techno.study>
2021-11-20 11:15:03 -0500

    Project - 5

---
+++ src/project5/task2/Task2.java
@@ -0,0 +1,22 @@
+package project5.task2;
+
+/*
+-	Create a class called Employee with following attributes:
+    -- baseSalary
+    -- hourlyRate
+    -- numberOfEmployees (this field should be increased every time you create a new employee object - you shouldn't have a copy of this field for each object)
+
+-	Create a constructor in Employee class to initialize baseSalary and hourlyRate
+    -- baseSalary cannot be less than or equal to zero
+    -- hourlyRate cannot be less than zero
+    -- If these conditions are not true while initializing baseSalary and hourlyRate throw an exception.
+    -- [I know you haven't learned exceptions yet but throwing an exception is easy thing, you can google it :)](A message from Mr. Halit!!!)
+
+-	Create a method in Employee class called calculateWage with a parameter of which is extraHours.
+    And this method should calculate the wage (wage = baseSalary + (hourlyRate * extraHours))
+
+-	Create a method in Employee class (named as getNumberOfEmployees) to return the number of employees (this method should be called using its class name.)
+
+ */
+public class Task2 {
+}
